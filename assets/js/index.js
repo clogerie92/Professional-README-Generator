0c14fbc184f10a0b5deb9ca4e65fc33f11812ca7
clogerie <66565287+clogerie@users.noreply.github.com>
2022-06-08 15:04:30 -0400

    Added function to prompt user README file questions, response is logged to console.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,4 +2,42 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-
+inquirer.prompt([
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your application?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please write a description of your application."
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "Please explain how to install your application."
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "Please explain how to use your application."
+    },
+    {
+        type: "input",
+        name: "Contribution",
+        message: "Please provide guidlines on how to contribute to your application."
+    },
+    {
+        type: "input",
+        name: "Test",
+        message: "Please explain how to test your application."
+    },
+    {
+        type: "list",
+        name: "License",
+        choices: ["Rutgers", "MIT", "GVL GPL License", "Appache License", "No License"]
+    }
+]).then((response) => {
+    console.log(response);
+});
